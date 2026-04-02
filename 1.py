6427ce82756f84e7675c446e70ea1240812e117b
harrypotter1808 <pkofficial0804@gmail.com>
2026-04-02 13:41:10 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-print("khandare",end=" ") # "end" is use to have the next line word within the current line
-print("prajwal")
-print("prajwal \n khandare")
-
-a=10
-print(a)
-print(type(a))

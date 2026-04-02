c90b9724629621c2a945cfb4e98a5bb34793f61f
harrypotter1808 <pkofficial0804@gmail.com>
2026-04-02 13:41:54 +0530

    Add files via upload
    hello world

---
+++ 1.py
@@ -0,0 +1,11 @@
+# print("hello world")
+# print("khandare",end=" ") # "end" is use to have the next line word within the current line
+# print("prajwal")
+# print("prajwal \n khandare")
+
+# a=10
+# print(a)
+# print(type(a))
+
+
+print("hello pk")
